b603e19d82e5b3c1c36158627c4b1dd7f5c2da0f
AurelioAlfons <yuroalfons0407@gmail.com>
2024-11-23 10:29:36 +1100

    Add Init State and counter button

--- lib/Project2/pages/choose_location.dart
+++ lib/Project2/pages/choose_location.dart
@@ -8,8 +8,32 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  int counter = 0;
+
+  void getData() {
+    // Simulate a network request for username
+    // Wait for 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      // ignore: avoid_print
+      print('Yoshi');
+    });
+  }
+
   @override
+  // Called once the widget is created
+  // Subscribe to streams or any object that could change our widget data
+  // init runs first
+  void initState() {
+    super.initState();
+    // ignore: avoid_print
+    print('inintState function ran');
+  }
+
+  @override
+  // Build the widget tree
   Widget build(BuildContext context) {
+    // ignore: avoid_print
+    print('build function ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -22,11 +46,31 @@ class _LocationState extends State<Location> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: const Center(
-          child: Text(
-        'Choose Location',
-        style: TextStyle(fontSize: 40),
-      )),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Center(
+              child: Text(
+            'Choose Location',
+            style: TextStyle(fontSize: 40),
+          )),
+          const SizedBox(
+            height: 50,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter += 1;
+                });
+              },
+              child: Text(
+                'Counter is $counter',
+                style: const TextStyle(fontSize: 35, color: Colors.black),
+              ))
+        ],
+      ),
     );
   }
 }
